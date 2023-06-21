8159450b6030a030dc541154afdbede180b3e5f3
Afolabi Opeyemi <yemijoshua81@gmail.com>
2023-06-21 08:03:58 +0100

    updating cors use method

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(cors)
+app.use(cors());
 
 const expressSession = require("cookie-session");
 app.use(
@@ -21,7 +21,6 @@ app.use(
         saveUninitialized: true,
     })
 );
-
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json({ limit: "10000mb" }));
